b648c45e38b7ae94359cc5fc9591e41bf60d3f00
Gustavo Watanabe <gustavo.watanabe@gmail.com>
2024-03-30 11:30:37 +0000

    Hello RUST

---
+++ hello-rust/target/debug/deps/hello_rust-18801d5e125aa0ce.d
@@ -0,0 +1,5 @@
+/home/gusw/code/rust_main/hello-rust/target/debug/deps/hello_rust-18801d5e125aa0ce: src/main.rs
+
+/home/gusw/code/rust_main/hello-rust/target/debug/deps/hello_rust-18801d5e125aa0ce.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/gusw/code/rust_main/hello-rust/target/debug/hello-rust: /home/gusw/code/rust_main/hello-rust/src/main.rs
